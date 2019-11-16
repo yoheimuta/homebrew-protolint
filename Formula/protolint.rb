8533a84927a45077a9d7f8235084ae3ccc99d954
yoheimuta <yoheimuta@gmail.com>
2019-11-16 13:51:21 +0900

    update v0.20.2

--- Formula/protolint.rb
+++ Formula/protolint.rb
@@ -5,19 +5,21 @@ class Protolint < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/yoheimuta/protolint/releases/download/v0.20.0/protolint_0.20.0_Darwin_x86_64.tar.gz"
-    sha256 "4839c4c041393d0323b62e673cd43d5176b57a9b46098570869f3874bbf488fe"
+    url "https://github.com/yoheimuta/protolint/releases/download/v0.20.2/protolint_0.20.2_Darwin_x86_64.tar.gz"
+    sha256 "19a4c5097de6ae486cf63a24965ea9c9a7b4a0a32c5fc6d692f2ee091168eaf8"
   elsif OS.linux?
-    url "https://github.com/yoheimuta/protolint/releases/download/v0.20.0/protolint_0.20.0_Linux_x86_64.tar.gz"
-    sha256 "abc293f2d1f106a26f63d054190c82516ec293035c6630cdc9493a5338da1042"
+    url "https://github.com/yoheimuta/protolint/releases/download/v0.20.2/protolint_0.20.2_Linux_x86_64.tar.gz"
+    sha256 "f7b4df1226d321ddc9c493ef9f5af8bbd775470d263556cfd5076d831326d79b"
   end
 
   def install
     bin.install "protolint"
+    bin.install "protoc-gen-protolint"
     prefix.install "LICENSE"
   end
 
   test do
     system "#{bin}/protolint version"
+    system "#{bin}/protoc-gen-protolint version"
   end
 end
