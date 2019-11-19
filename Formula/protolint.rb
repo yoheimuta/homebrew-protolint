3a54879d91973d6a960611cc777ac627dd6aa5d2
yoheimuta <yoheimuta@gmail.com>
2019-11-20 08:00:52 +0900

    feat: Update to v0.21.0

--- Formula/protolint.rb
+++ Formula/protolint.rb
@@ -1,15 +1,15 @@
 class Protolint < Formula
   desc "A pluggable tool to enforce Protocol Buffer style and conventions. "
   homepage "https://github.com/yoheimuta/protolint"
-  version "v0.20.0"
+  version "v0.21.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/yoheimuta/protolint/releases/download/v0.20.2/protolint_0.20.2_Darwin_x86_64.tar.gz"
-    sha256 "19a4c5097de6ae486cf63a24965ea9c9a7b4a0a32c5fc6d692f2ee091168eaf8"
+    url "https://github.com/yoheimuta/protolint/releases/download/v0.21.0/protolint_0.21.0_Darwin_x86_64.tar.gz"
+    sha256 "2f156bbb4386de3bcaa8a8e3a44db9f2a300ab186743cb81a9081f9d6fd69a36"
   elsif OS.linux?
-    url "https://github.com/yoheimuta/protolint/releases/download/v0.20.2/protolint_0.20.2_Linux_x86_64.tar.gz"
-    sha256 "f7b4df1226d321ddc9c493ef9f5af8bbd775470d263556cfd5076d831326d79b"
+    url "https://github.com/yoheimuta/protolint/releases/download/v0.21.0/protolint_0.21.0_Linux_x86_64.tar.gz"
+    sha256 "6cd2acf0ca0fa5519ec5e3e29cc81c78a0e2d06d4b6d924427562c2779a70715"
   end
 
   def install
