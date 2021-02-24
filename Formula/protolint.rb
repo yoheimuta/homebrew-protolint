27ac76f8631abeddf5a80132a109c0f594aea747
goreleaserbot <yoheimuta@gmail.com>
2021-02-25 07:21:52 +0900

    Brew formula update for protolint version v0.29.0

--- Formula/protolint.rb
+++ Formula/protolint.rb
@@ -5,20 +5,20 @@
 class Protolint < Formula
   desc "Pluggable tool to enforce Protocol Buffer style and conventions"
   homepage "https://github.com/yoheimuta/protolint"
-  version "0.28.2"
+  version "0.29.0"
   bottle :unneeded
 
-  if OS.mac?
-    url "https://github.com/yoheimuta/protolint/releases/download/v0.28.2/protolint_0.28.2_Darwin_x86_64.tar.gz"
-    sha256 "16ac497aadcdaa498be14f6974db453db5452dffd74ddb7d493d52762adab61d"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/yoheimuta/protolint/releases/download/v0.29.0/protolint_0.29.0_Darwin_x86_64.tar.gz"
+    sha256 "a0cfb543817f1271b3d4808f50de8f6f7503d8c416aeb129e4e1d4d482396997"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/yoheimuta/protolint/releases/download/v0.28.2/protolint_0.28.2_Linux_x86_64.tar.gz"
-    sha256 "1183793536ddc36b33bf19e822db41ce9e9e835a69b02d549c9fcc5c4cbcc6d7"
+    url "https://github.com/yoheimuta/protolint/releases/download/v0.29.0/protolint_0.29.0_Linux_x86_64.tar.gz"
+    sha256 "77c2369b3d6054910a1178745c9efd289193a523aa148cbdc659e6e8ee75020a"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/yoheimuta/protolint/releases/download/v0.28.2/protolint_0.28.2_Linux_arm64.tar.gz"
-    sha256 "fce4cb6727b2c9621fcc2d7f2006be6e5ab12482b64ba0965d7a0972e5483163"
+    url "https://github.com/yoheimuta/protolint/releases/download/v0.29.0/protolint_0.29.0_Linux_arm64.tar.gz"
+    sha256 "d57fd45ab9cd6b0ee01017afd4f44a78ea212b5c4eec47deb6283fbe24c0c6b9"
   end
 
   def install
